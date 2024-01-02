7074fc07d856b42e92398ad08c790be85f3c16e8
Daniel Penaloza <juandanielpenalozabrito@gmail.com>
2024-01-01 21:09:26 -0500

    feat: :sparkles: Add dome dependencies and add a domain

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'tlr.stripocdn.email',
+                port: '',
+                pathname: '/**',
+            },
         ],
     },
 };
