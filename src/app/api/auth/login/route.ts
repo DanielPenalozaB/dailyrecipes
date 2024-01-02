5fe8187fd24ceac1d38d907154f9555e6fa7b018
Daniel Penaloza <juandanielpenalozabrito@gmail.com>
2024-01-01 21:12:08 -0500

    feat: :poop: Adding some code to structure the basic endpoints of login and register

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,72 @@
-export async function POST(request: Request) {
-    const body = await request.json();
-    console.log(body);
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
 
-    return new Response(JSON.stringify({ ok: true }));
+export async function POST(request: Request) {
+    try {
+        const prisma = new PrismaClient();
+
+        const body = await request.json();
+
+        if (body.email && body.password) {
+            const findUser = await prisma.user.findFirst({
+                where: {
+                    email: body.email,
+                },
+            });
+
+            if (findUser) {
+                if (findUser.password !== body.password) {
+                    return NextResponse.json(
+                        {
+                            status: 'error',
+                            message: 'Incorrect password.',
+                        },
+                        {
+                            status: 400,
+                        }
+                    );
+                } else {
+                    return NextResponse.json(
+                        {
+                            status: 'success',
+                            message: 'Login successful.',
+                        },
+                        {
+                            status: 200,
+                        }
+                    );
+                }
+            } else {
+                return NextResponse.json(
+                    {
+                        status: 'error',
+                        message: 'Email not found.',
+                    },
+                    {
+                        status: 400,
+                    }
+                );
+            }
+        } else {
+            return NextResponse.json(
+                {
+                    status: 'error',
+                    message: 'Invalid request. Please provide valid data.',
+                },
+                {
+                    status: 400,
+                }
+            );
+        }
+    } catch (error) {
+        return NextResponse.json(
+            {
+                status: 'error',
+                message: 'Internal server error. Please try again later.',
+            },
+            {
+                status: 500,
+            }
+        );
+    }
 }
