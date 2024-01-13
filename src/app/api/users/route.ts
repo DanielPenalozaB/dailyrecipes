668395b7eb103c36d1d21a876f328c36bdec481e
Daniel Penaloza <juandanielpenalozabrito@gmail.com>
2024-01-13 14:11:02 -0500

    feat: :green_heart: Change '/api/users' route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,9 +2,10 @@ import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const prisma = new PrismaClient();
+    return NextResponse.json({ name: 'John Doe' });
+    /*const prisma = new PrismaClient();
 
     const users = await prisma.user.findMany();
 
-    return NextResponse.json(users);
+    return NextResponse.json(users);*/
 }
